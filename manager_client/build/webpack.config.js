c3d7946c2fe2b0a9987f8c05dd7864bf0e3f7545
walker <565828928@qq.com>
2019-08-06 10:23:26 +0800

    mobile update

--- manager_client/build/webpack.config.js
+++ manager_client/build/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
         contentBase: false,
         compress: true,
         hot: true,
-        port: 7070,
+        port: 8000,
         proxy: {
             '/': {
                 target: 'http://127.0.0.1:8080/',
